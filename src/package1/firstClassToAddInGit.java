38a566674b2d2dd6efa0511d95051544b2ab0899
claudia <grigori_claudia@mail.com>
2012-12-05 13:52:33 +0200

    firstClass committed

---
+++ src/package1/firstClassToAddInGit.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class firstClassToAddInGit {
+
+	private String depedency;
+	
+	public firstClassToAddInGit() {
+		this.depedency = "nothing";
+	}
+	
+	public String getDepedency() {
+		return depedency;
+	}
+	
+	public void setDepedency(String depedency) {
+		this.depedency = depedency;
+	}
+}
